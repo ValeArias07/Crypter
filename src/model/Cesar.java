47f6ad7e33c9765dd5f6892988f7303a16c23828
JLRO1812 <restrepitoj.l@gmail.com>
2020-04-29 20:02:10 -0500

    Alphabet arrays have been created

--- src/model/Cesar.java
+++ src/model/Cesar.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Cesar {
+	
+	char abcMinus [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char abcMayus [] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	
+	public static String cesarCrypter() {
+		return null;
+		
+	}
 
 }
