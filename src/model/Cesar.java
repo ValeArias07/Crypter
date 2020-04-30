792888faf0d2f5117cae0685e3cb36830fb52da6
JLRO1812 <restrepitoj.l@gmail.com>
2020-04-29 23:17:22 -0500

    Advance of encryption by the Cesar method in the Cesar class

--- src/model/Cesar.java
+++ src/model/Cesar.java
@@ -2,12 +2,72 @@ package model;
 
 public class Cesar {
 	
-	char abcMinus [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
-	char abcMayus [] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	public final String ABC = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";	
 	
-	public static String cesarCrypter() {
-		return null;
+	private int track; //cantidad de letras a mover
+	private String direction; //right(R), left(L)
+
+	public Cesar(int track, String direction) {
+		this.track=track;
+		this.direction=direction;
+	}
+	
+	public String encrypt(String txt) {
 		
+		String crypter; 
+		
+		if(direction.equals("R")) {
+			
+			crypter = rigthMove(txt);
+		
+		}else {
+			
+			crypter = leftMove(txt);
+			
+		}return crypter;
+		
+	}
+	
+	private String leftMove(String txt) {
+		
+		String crypter = "";
+		
+		for(int i = 0; i<txt.length(); i++) {
+			int ward = ABC.indexOf(txt.charAt(i));
+
+				if(ward>=0) {
+					if((ward-track)>=0) {
+						crypter = crypter + ABC.charAt(ward-track);
+					}else {
+						ward = (ward-track)+(ABC.length()-1);
+						crypter = crypter + ABC.charAt(ward);					}
+						
+				}else {
+					crypter = crypter + txt.charAt(i);
+				}
+			
+			}return crypter;
+	}
+
+	public String rigthMove(String txt) {
+		
+		String crypter = "";
+		
+		for(int i = 0; i<txt.length(); i++) {
+			int ward = ABC.indexOf(txt.charAt(i));
+
+				if(ward>=0) {
+					if((ward+track)<ABC.length()) {
+						crypter = crypter + ABC.charAt(ward+track);
+					}else {
+						ward = (ward+track)-(ABC.length()-1);
+						crypter = crypter + ABC.charAt(ward);					}
+						
+				}else {
+					crypter = crypter + txt.charAt(i);
+				}
+			
+			}return crypter;
 	}
 
 }
