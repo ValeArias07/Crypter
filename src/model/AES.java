87eacd00b28970ff949e07aa0e8c949a2da7eb60
JLRO1812 <restrepitoj.l@gmail.com>
2020-05-03 10:07:35 -0500

    The methods and tests of the AES class have been created

--- src/model/AES.java
+++ src/model/AES.java
@@ -1,20 +1,42 @@
 package model;
 
 public class AES extends Polyalphabetic{
+	
+	public final String [][] SBOX = {};
+	public final int [][] CONSTANTMATRIZ = {{02,03,01,01},{01,02,03,01},{01,01,02,03},{03,01,01,02}};
+	
+	private String txt;
 
-	public AES(String encriptionKey) {
+	public AES(String encriptionKey, String txt) {
 		super(encriptionKey);
+		this.txt = txt;
 	}
 
 	@Override
 	public String encrypt(String txt) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public String decrypt(String txt) {
-		// TODO Auto-generated method stub
+		
+		return null;
+	}
+	
+	public String [][] subBytes(String [][] state){
+		return state;
+	}
+	
+	public String [][] shiftRows(String [][] state){
+		return state;
+	}
+	
+	public String [][] mixColums(String [][] state){
+		return state;
+	}
+	
+	public String addRoundKey(String [][] state){
 		return null;
 	}
 
